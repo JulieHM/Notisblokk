0d4f81722047acf709e8b2201999d7793589a7ae
Thomas <thomasdh@stud.ntnu.no>
2019-09-04 20:32:01 +0200

    Add test for toString

--- notisblokk/src/test/java/notisblokk/model/NoteTest.java
+++ notisblokk/src/test/java/notisblokk/model/NoteTest.java
@@ -1,5 +1,17 @@
 package notisblokk.model;
 
+import java.time.LocalDateTime;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class NoteTest {
 
+  @Test
+  public void testToString() {
+    final LocalDateTime time = LocalDateTime.now();
+    final Note note = new Note("title", "msg", time, time);
+    String correctToString = "Note{title='title', message='msg', "
+        + "lastEditedDate=" + time + ", createdDate=" + time + "}";
+    Assert.assertEquals(correctToString, note.toString());
+  }
 }
