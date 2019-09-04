4315c84b8bdc90459544d9ffc0402503d85b3abc
Thomas <thomasdh@stud.ntnu.no>
2019-09-04 21:32:48 +0200

    Add test for appending a note

--- notisblokk/src/test/java/notisblokk/model/NotesTest.java
+++ notisblokk/src/test/java/notisblokk/model/NotesTest.java
@@ -4,9 +4,11 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
 public class NotesTest {
 
   private Notes notes;
+
   @Before
   public void init() {
     notes = new Notes();
@@ -15,5 +17,13 @@ public class NotesTest {
   @Test
   public void testEmptyConstructor() {
     Assert.assertEquals(0, notes.getNumNotes());
+  }
+
+  @Test
+  public void testAddNote() {
+    notes.addNote(new Note("title", "msg"));
+    Assert.assertEquals(1, notes.getNumNotes());
+    notes.addNote(new Note("other title", "other msg"));
+    Assert.assertEquals(2, notes.getNumNotes());
   }
 }
