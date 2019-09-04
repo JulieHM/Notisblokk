696e2881dda0223636f50300592d0512863bb6de
Thomas <thomasdh@stud.ntnu.no>
2019-09-04 21:32:06 +0200

    Add empty constructor test

--- notisblokk/src/test/java/notisblokk/model/NotesTest.java
+++ notisblokk/src/test/java/notisblokk/model/NotesTest.java
@@ -1,5 +1,19 @@
 package notisblokk.model;
 
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 public class NotesTest {
 
+  private Notes notes;
+  @Before
+  public void init() {
+    notes = new Notes();
+  }
+
+  @Test
+  public void testEmptyConstructor() {
+    Assert.assertEquals(0, notes.getNumNotes());
+  }
 }
