b8df2c05f1983e260272ed3e54648c17707fe725
Thomas <thomasdh@stud.ntnu.no>
2019-09-04 20:12:12 +0200

    Create Notes.java

---
+++ notisblokk/src/main/java/notisblokk/model/Notes.java
@@ -0,0 +1,58 @@
+package notisblokk.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class Notes implements Iterable<Note> {
+
+  private List<Note> notes = new ArrayList<>();
+
+  public Notes() {
+    // Possible to create an empty object of Notes
+  }
+
+  public Notes(Note note) {
+    addNote(note);
+  }
+
+  public Notes(Collection<Note> notes) {
+    addNotes(notes);
+  }
+
+  @Override
+  public Iterator<Note> iterator() {
+    return notes.iterator();
+  }
+
+  public void addNote(Note note) {
+    // NOTE: possibly return the index it is placed in?
+    notes.add(note);
+  }
+
+  public void addNotes(Collection<Note> notes) {
+    // NOTE: possibly return the index it is placed in?
+    this.notes.addAll(notes);
+  }
+
+  public void removeNote(Note note) {
+    notes.remove(note);
+  }
+
+  public void removeNote(int index) {
+    notes.remove(index);
+  }
+
+  public Note getNote(int index) {
+    return notes.get(index);
+  }
+
+  public List<Note> getNotes() {
+    return notes;
+  }
+
+  public int getNumNotes() {
+    return notes.size();
+  }
+}
