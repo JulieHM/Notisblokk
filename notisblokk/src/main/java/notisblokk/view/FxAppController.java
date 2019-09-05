342f97c89f42d3364aef9627842eb3ca27111f5f
Benedicte Myrvoll <benny@dhcp-10-24-6-27.wlan.ntnu.no>
2019-09-05 11:26:42 +0200

    Add methods

---
+++ notisblokk/src/main/java/notisblokk/view/FxAppController.java
@@ -0,0 +1,39 @@
+package notisblokk.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class FxAppController {
+
+  @FXML
+  private Button saveButton;
+
+  @FXML
+  private Button newNote;
+
+  @FXML
+  private TextField titleField;
+
+  @FXML
+  private TextField noteText;
+
+  @FXML
+  private VBox noteContainer;
+
+
+  private void newNote() {
+    
+  }
+
+  private void saveNote() {
+
+  }
+
+  private void openNote() {
+
+  }
+
+}
