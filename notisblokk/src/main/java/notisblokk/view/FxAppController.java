0dfbfab2868e13dd51de27fa4844a6e164756353
Benedicte Myrvoll <benny@Benedictes-MacBook-Air.local>
2019-09-06 17:52:45 +0200

    Add method onNewNoteClick

--- notisblokk/src/main/java/notisblokk/view/FxAppController.java
+++ notisblokk/src/main/java/notisblokk/view/FxAppController.java
@@ -1,15 +1,19 @@
 package notisblokk.view;
 
+import java.awt.Insets;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import notisblokk.model.Note;
 
 public class FxAppController {
 
   @FXML
-  private Button saveButton;
+  private Button saveNote;
 
   @FXML
   private Button newNote;
@@ -18,21 +22,29 @@ public class FxAppController {
   private TextField titleField;
 
   @FXML
-  private TextField noteText;
+  private TextArea noteText;
 
   @FXML
   private VBox noteContainer;
 
+  @FXML
+  private HBox singleNoteContainer;
 
+  @FXML
+  private Label savedNote;
+
+  @FXML
+  private void onNewNoteClick() {
+    titleField.setText("");
+    noteText.setText("");
+
+    Label label = new Label("New note");
+
+    HBox hbox = new HBox(label);
+    noteContainer.getChildren().add(hbox);
+  }
+  @FXML
   private void onSaveClick() {
-    
-  }
-
-  private void saveNote() {
-
-  }
-
-  private void openNote() {
 
   }
 
