e4c0a6b664666932d4874f148c2339a0c1b3fc88
Karl Petter Aubert <kpaubert@stud.ntnu.no>
2019-10-08 10:37:52 +0200

    Create tests for noteservice

--- notisblokk/restapi/src/main/java/notiskblokk/restapi/NotesSpringBootApp.java
+++ notisblokk/restapi/src/main/java/notiskblokk/restapi/NotesSpringBootApp.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotesSpringBootApp {
-
   public static void main(String[] args) {
     SpringApplication.run(NotesSpringBootApp.class, args);
   }
--- notisblokk/restapi/src/test/java/notisblokk/restapi/NoteServiceTest.java
+++ notisblokk/restapi/src/test/java/notisblokk/restapi/NoteServiceTest.java
@@ -1,36 +1,66 @@
 package notisblokk.restapi;
 
-import java.util.List;
 import java.io.IOException;
 import notisblokk.core.Note;
-import notisblokk.json.NoteDeserializer;
+import notisblokk.core.Notes;
 import notiskblokk.restapi.NoteService;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class NoteServiceTest {
 
-  public NoteService noteService = new NoteService();
-  private Note replacementNote = new Note("Replacement note", "Replacement note");
+  private static NoteService noteService = new NoteService();
+  private static Notes backupNotes = new Notes();
+
+  @BeforeClass
+  public static void beforeInit() {
+
+  }
 
   @Before
   public void init() {
-    System.out.println("Start test");
   }
 
   @Test
-  public void noteServiceTest() {
+  public void testGetAllNotes() {
+    Notes notes = noteService.getAllNotes();
+    Assert.assertEquals(notes, noteService.getAllNotes());
+  }
+
+  @Test
+  public void testReplaceNote() {
+    Note replacementNote = new Note("Replacement note", "Replacement note");
     noteService.addNote(new Note("Some title", "Some title"));
     int length = noteService.getAllNotes().getNumNotes();
-    noteService.replaceNote(length-1, replacementNote);
-    Assert.assertEquals(replacementNote, noteService.getNote(length-1));
+    noteService.replaceNote(length - 1, replacementNote);
+    Assert.assertEquals(replacementNote, noteService.getNote(length - 1));
+    // Remove note after test
+    noteService.removeNote(length - 1);
+  }
+
+  @Test
+  public void addNote() {
+    Note note = new Note("Add note title", "Add note message");
+    noteService.addNote(note);
+    int length = noteService.getAllNotes().getNumNotes();
+    Assert.assertEquals(note, noteService.getNote(length - 1));
+    // Remove note after test
+    noteService.removeNote(length - 1);
+  }
+
+  @Test
+  public void getNote() {
+    int length = noteService.getAllNotes().getNumNotes();
+    Note note = noteService.getNote(length - 1);
+    Assert.assertEquals(note, noteService.getNote(length - 1));
   }
 
   @After
-  public void after(){
-    System.out.println("End test");
+  public void after() {
+    System.out.println("Finished.");
   }
 
   public NoteServiceTest() throws IOException {
